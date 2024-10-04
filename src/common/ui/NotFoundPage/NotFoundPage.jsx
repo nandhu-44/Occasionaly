bdd2db5383fca2d181f03d74562606f1aa479ddf
nandhu-44 <nandhu90040@gmail.com>
2024-10-04 10:23:26 +0530

    Made a simple 404 page

---
+++ src/common/ui/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,41 @@
+import Footer from "@/common/components/Footer";
+import Navbar from "@/common/components/Navbar";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <>
+      <Navbar />
+      <main className="flex h-screen flex-col items-center justify-center px-4 text-center">
+        <div className="max-w-md">
+          <Image
+            src="/404-Illustration.svg"
+            alt="404"
+            width={300}
+            height={300}
+            className="mx-auto"
+            priority
+            style={{
+              layout: "fixed",
+            }}
+          />
+          <h1 className="mt-6 text-3xl font-bold text-gray-800 md:text-4xl">
+            404 - Page Not Found
+          </h1>
+          <p className="mt-4 text-left text-gray-600 lg:text-center">
+            The page you are looking for might have been removed, had its name
+            changed, or is temporarily unavailable.
+          </p>
+          <Button className="mt-8">
+            <Link href="/">Go Home</Link>
+          </Button>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default NotFoundPage;
