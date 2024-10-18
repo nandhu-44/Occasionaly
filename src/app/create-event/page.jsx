52142bc3b82456d0fa885ca2ab45a81e6f555f5d
nandhu-44 <nandhu90040@gmail.com>
2024-10-18 09:31:42 +0530

    Resolving conflicts

--- src/app/create-event/page.jsx
+++ src/app/create-event/page.jsx
@@ -1,14 +1,6 @@
 import React from "react";
-import Navbar from "@/common/components/Navbar";
-import Footer from "@/common/components/Footer";
 import EventForm from "@/common/ui/EventsPage/EventForm";
 
 export default function CreateEventPage() {
-  return (
-    <>
-      <Navbar />
-      <EventForm />
-      <Footer />
-    </>
-  );
+  return <EventForm />;
 }
--- src/common/ui/HomePage/HomePage.jsx
+++ src/common/ui/HomePage/HomePage.jsx
@@ -14,7 +14,6 @@ const HomePage = () => {
     <>
       <Header />
       <ActiveEvents filters={defaultFilters} />
-      <Footer />
     </>
   );
 };
